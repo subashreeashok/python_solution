dbd8bb9b3e439b1925392a4484d2c2b70c3a2714
subashreeashok <33508353+subashreeashok@users.noreply.github.com>
2017-12-06 16:57:55 +0530

    Add files via upload

---
+++ python_set1/q1_4c.py
@@ -0,0 +1,14 @@
+
+
+
+st = 6 * 3600 + 52 *60 
+e = 2 * (8 * 60 + 15 ) 
+t = 3 * (7 * 60 + 12 )
+tt = e + t + st
+h= tt/ 3600 
+s= tt % 3600 
+m = s /60 
+se= s % 60 
+print 'Hours:',h 
+print 'minutes:', m
+print 'seconds:', se 
